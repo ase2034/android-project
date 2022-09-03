66e3630cb8f85399691a7a87ff1db80682265cfb
ase2034 <3200105336@zju.edu.cn>
2022-09-03 15:17:25 +0800

    first commit

---
+++ QuickStart_Demo/app/src/main/java/com/ss/rtc/demo/quickstart/viewpagerActivity.java
@@ -0,0 +1,14 @@
+package com.ss.rtc.demo.quickstart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class viewpagerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_viewpager);
+    }
+}
